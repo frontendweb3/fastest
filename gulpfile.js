8ef563a7b03aaaa2c39b1d6f70691d1b4bd80791
officialrajdeepsingh <radewala.2@gmail.com>
2022-12-22 21:11:47 +0530

    remove css config in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,8 @@
 var gulp = require('gulp');
-var cssnano = require('gulp-cssnano'); 
 var concat = require('gulp-concat'); 
 var uglify = require('gulp-uglify');
 
 const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-
-
-
-gulp.task('css', function(){    
-    return gulp.src(['assets/css/editor.css','assets/css/tailwind.css'])  
-    .pipe(sourcemaps.init())
-		.pipe(autoprefixer())           
-        .pipe(cssnano())       
-		.pipe(concat('build.css'))
-		.pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/build/css')); 
-});
-
 
 gulp.task('main', function(){    
     return gulp.src('assets/js/style.js') 
@@ -28,11 +13,3 @@ gulp.task('main', function(){
         .pipe(gulp.dest('assets/build/js')); 
 });
 
-gulp.task('search', function(){    
-    return gulp.src('assets/js/search.js') 
-    .pipe(sourcemaps.init())          
-        .pipe(concat('search.js'))       
-        .pipe(uglify())    
-        .pipe(sourcemaps.write('.'))   
-        .pipe(gulp.dest('assets/build/js')); 
-});
