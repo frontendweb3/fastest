a44138ec292231bf9a8d9c5c8836308764565cbf
Rajdeep Singh <radewala.2@gmail.com>
2026-03-29 17:32:14 +0530

    feat: add Lucide icons initialization script

---
+++ assets/js/icons.js
@@ -0,0 +1,38 @@
+// Initialize Lucide icons - include only the icons you need
+
+import { createIcons, Sun, Moon, Search,UserRound, SendHorizontal, House, ChevronRight,ChevronLeft, PanelLeft, CircleUserRound, Rss, Hash, Dot, ArrowRight, BadgeCheck, Copy, CopyCheck, UserPlus, Globe, Crown, Tag, Lock, MoveRight } from 'lucide';
+export function initIcons() {
+console.log("Lucide Sun and Moon icons initialized:", Sun, Moon );
+  createIcons({
+    icons: {
+      Sun,
+      Moon,
+      Search,
+      UserRound,
+      SendHorizontal,
+      House,
+      ChevronRight,
+      ChevronLeft,
+      PanelLeft,
+      CircleUserRound,
+      Rss,
+      Hash,
+      Dot,
+      ArrowRight,
+      BadgeCheck,
+      Copy,
+      CopyCheck,
+      UserPlus,
+      Globe,
+      Crown,
+      Tag,
+      Lock,
+      MoveRight,
+    }
+  });
+}
+
+// Auto-initialize icons
+initIcons();
+
+// {{> "components/icon" name="search" ariaLabel="search icon"}}
